18a2856c962a2d899ded9f5a5ba0ed13f6e8a080
SabrinaBarros <sabrina.barros2045@gmail.com>
2024-03-20 21:23:14 -0300

    feat: dynamic render of projects from json

--- vite-project/src/views/Projects/Projects.jsx
+++ vite-project/src/views/Projects/Projects.jsx
@@ -1,45 +1,27 @@
 import './projects.css'
 import Card from '../../components/Card/Card.jsx'
+import projectsDB from "../../database/projects.json"
 
 const Projects = () => {
+
+  const $projects = projectsDB.projects.map((project) => {
+    return (
+      <Card
+        name={project.name}
+        imagePath={project.imagePath}
+        tag={project.tag}
+        gitHubURL={project.gitHubURL}
+        demoURL={project.demoURL}
+        description= {project.description}
+        key={project._id}
+      />
+    )
+  })
 
   return (
     <main className="projects">
 
-      <Card
-        name="Site Pessoal"
-        imagePath="img/projects/website.png"
-        tag="app"
-        gitHubURL="https://github.com/SabrinaBarros/sabrinabarros.github.io"
-        demoURL="/"
-        description="🌈Esse maravilhoso Website.✨"
-      />
-
-      <Card
-        name="React Pomodoro"
-        imagePath="img/projects/pomodoroTimer.png"
-        tag="app"
-        gitHubURL="https://github.com/SabrinaBarros/pomodoroTimer?tab=readme-ov-file"
-        demoURL="https://sabrinabarros.github.io/pomodoroTimer/"
-        description="Cronômetro intuitivo construído com React, seguindo a técnica Pomodoro. 🍅"
-      />
-
-      <Card
-        name="Ditto Boilerplate"
-        imagePath="img/projects/ditto.png"
-        tag="boilerplate"
-        gitHubURL="https://github.com/SabrinaBarros/ditto-boilerplate"
-        demoURL="https://sabrinabarros.github.io/ditto-boilerplate/src/index.html"
-        description="⚙️ Boilerplate Front-End simples e elegante, desenvolvido com HTML e CSS vanilla. ☂️"
-      />
-
-      <Card
-        name="Kadabra"
-        imagePath="img/projects/kadabra.png"
-        tag="cli"
-        gitHubURL="https://github.com/SabrinaBarros/kadabra"
-        description="Ferramenta que utiliza a Cifra Vigenere para criptografar textos e senhas."
-      />
+      {$projects}
 
     </main>
   )
